711d00e26cb8dbaada07a83005434cf02c0294d0
asingh250 <81792274+asingh250@users.noreply.github.com>
2021-05-02 21:50:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,9 @@
 #include <iostream>
-#include "include/salesManager.h"
-
-
+#include "include/salesManager.h”
+ 
+ 
 int main() {
-	loadInfo();
- return 0;
-}
-/*
-
-Pseudocode:
-
-	Classes:
-
-		Clients - name, short address.
-		SalesReps - name, short address, calculated sales bonus.
-		Products - product/service name, and any current discount. 
-		Sales - product, date, client, salesRep.
-
-	salesManager:
-
-		vector <Client*> clients;
-		vector <SalesRep*> salesReps;
-		vector <Product*> products;
-		vector <Sale*> Sales;
-
-		retrieveInfo(){ ...retrieve file info and populate vectors… }
-		updateInfo(){ ...write all info from vector objects to files… }
-		listAll(){ ...prints info for all items in a specified vector… }
-		listIndividual(){ ...lists info on a specific element in a specific vector… }
-		monthlySales(){ ...generate monthly sales report from sales vector…}
-		purchase(){ ... input info to make new sale object and append to Sales vector… }
-
-		printMenu(){ 
-		(use try/throw blocks)
-		...print the menu and wait for response… 
-		…uses above functions in switch statements to display/edit data... }
-		main(){
-		...call print menu in while loop...
-		}
-
-	Main:
-		loadInfo();
-		printMenu();
-*/
+    //loads info from text files
+    retrieveInfo();
+    //prints the menu and takes commands until q is typed
+    printMenu();
